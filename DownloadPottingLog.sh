a8937ac1625987001ac0d5f88012749e6ee00583
Caleb Fangmeier <cfangmeier74@gmail.com>
2016-04-01 14:23:35 -0500

    some rearranging of the potting data analysis files as well as introduction of a script to automate creating the json file

---
+++ DownloadPottingLog.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ssh $1@t3.unl.edu "zip -uj Potting_Logs.zip /home/dominguez/cfangmeier/elog/elog-2.9.2/logbooks/SiLab_Logbook/*_Config-*-*-*.zip"
+scp $1@t3.unl.edu:~/Potting_Logs.zip .
+./PottingLog2JSON.py Potting_Logs.zip
+if [ $? -eq 0 ]
+then
+    rm Potting_Logs.zip
+    echo "Finished creating JSON file for potting data"
+else
+    echo "Error parsing Logfiles"
+fi
